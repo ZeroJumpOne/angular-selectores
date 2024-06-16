09b8bef01aab818e589903e93e68a325917e4a77
M.Ing. Isaac Hdez Castro <isaac@zerojumpone.mx>
2024-06-16 17:17:18 -0600

    cambio de rutas

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -3,22 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { FrmSelectorComponent } from './pages/frm-selector/frm-selector.component';
 
 const routes: Routes = [
-   // {
-   //    path: '',
-   //    component: FrmSelectorComponent,
-   // },
-   // {
-   //    path: '**',
-   //    redirectTo: 'selector',
-   // }
    {
       path: '',
-      children: [
-         { path: 'selector', component: FrmSelectorComponent },
-         { path: '**',       redirectTo: 'selector' },
-      ]
-
+      component: FrmSelectorComponent,
+   },
+   {
+      path: '**',
+      redirectTo: 'selector',
    }
+   // {
+   //    path: '',
+   //    children: [
+   //       { path: 'selector', component: FrmSelectorComponent },
+   //       { path: '**',       redirectTo: 'selector' },
+   //    ]
+
+   // }
 ];
 
 @NgModule({
